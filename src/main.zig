6f9e158bca1df3f6adcc039b66b593d6105cefce
Sudarsh1010 <sudarshjain1010@gmail.com>
2025-01-19 13:15:49 +0530

    defer free memory

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ const Base64 = @import("base64.zig").Base64;
 pub fn main() !void {
     var memory_buffer: [1000]u8 = undefined;
     var fba = std.heap.FixedBufferAllocator.init(&memory_buffer);
-    const allocator = fba.allocator();
+    defer fba.reset();
 
     const stdout = std.io.getStdOut().writer();
 
@@ -13,8 +13,12 @@ pub fn main() !void {
 
     const base64 = Base64.init();
 
+    const allocator = fba.allocator();
     const encoded_text = try base64.encode(allocator, text);
     const decoded_text = try base64.decode(allocator, etext);
+
+    defer allocator.free(encoded_text);
+    defer allocator.free(decoded_text);
 
     try stdout.print("encoded text: {s}\n", .{encoded_text});
     try stdout.print("decoded text: {s}\n", .{decoded_text});
